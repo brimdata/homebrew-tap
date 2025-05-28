4167c399ee54a03ad01996d07bd3565aea979532
Phil Rzewski <phil@brimdata.io>
2025-05-28 12:10:29 -0700

    Update Homebrew formulae (#8)

--- super.rb
+++ super.rb
@@ -1,9 +1,9 @@
 class Super < Formula
   desc "Query and search data in files or SuperDB data lakes"
   homepage "https://superdb.org"
-  url "https://github.com/brimdata/super/archive/da56639.zip"
-  sha256 "b44eae9937fef284a3a1cb943a450c8acd702328bfdb99431abf59978b63f367"
-  version "da56639"
+  url "https://github.com/brimdata/super/archive/0cb66f4.zip"
+  sha256 "24d7afc4a430744fd65f01a89b01553bb5d895d990771199fa9b4e415ab66edb"
+  version "0cb66f4"
 
   depends_on "go@1.24" => :build
 
@@ -11,7 +11,7 @@ class Super < Formula
     ENV["GOPATH"] = buildpath
     (buildpath/"build/src").mkpath
     ln_s buildpath, buildpath/"build/src/github.com"
-    system "GOPATH=$PWD/build go install github.com/brimdata/super/cmd/super@da56639"
+    system "GOPATH=$PWD/build go install github.com/brimdata/super/cmd/super@0cb66f4"
     bin.install "build/bin/super"
   end
 end
